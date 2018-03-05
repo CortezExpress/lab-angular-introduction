b15c5e0d281eb4740b11232004ec5f6c58a11ae9
MARTIN GENET <martingenet@MHG.local>
2018-03-05 16:57:38 -0500

    iteration 1 done

--- starter-code/iteration1/src/app/app.component.ts
+++ starter-code/iteration1/src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  pictures: Array<Object> = [
+    {
+      src: "assets/images/picture.png",
+      description: 'picture 1'
+    }, {
+      src: "assets/images/picture.png",
+      description: 'picture 1 again',      
+    }, {
+      src: "assets/images/picture.png",
+      description: 'picture 1 a third time',     
+    }
+  ];
 }
